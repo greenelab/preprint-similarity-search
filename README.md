4fe14503ece7e1da5b96d616fa0fd21fbdb231dc
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-08-04 23:30:24 -0400

    add basic cloud image links

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AnnoRxiver Journal Recommender
 
-Enter a BioRxiv pre-print and get a list of journals that would be most suited to publish your paper.
+Enter a BioRxiv preprint and get a list of journals that would be most suited to publish your paper.
 Available as a web app and a server.
 
 [⭐ OPEN THE APP ⭐](https://greenelab.github.io/annorxiver-journal-recommender/)
--- scripts.js
+++ scripts.js
@@ -1,17 +1,15 @@
-// backend server
+// resources
 let server = 'https://api-journal-rec.greenelab.com/doi/';
-
-// map data
-let mapData = 'data/pmc_square_plot.json';
-
-// rank color
-let rankColor = '#ff9800';
-
-// lookup resources
+let mapData = 'data/plot.json';
 const googleLink = 'https://www.google.com/search?q=';
 const paperLink = 'https://www.ncbi.nlm.nih.gov/pmc/articles/';
 const metaLookup =
   'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pmc&tool=AnnoRxivir&email=greenescientist@gmail.com&retmode=json&id=';
+const cloudImages =
+  'https://raw.githubusercontent.com/greenelab/annorxiver/master/biorxiv/pca_association_experiment/output/word_pca_similarity/figures/pca_XX_cossim_word_cloud.png';
+
+// settings
+let rankColor = '#ff9800';
 
 // dom elements
 let searchForm = document.querySelector('#search');
@@ -23,10 +21,15 @@ let errorMessage = document.querySelector('#error_message');
 let journalsSection = document.querySelector('#journals_section');
 let papersSection = document.querySelector('#papers_section');
 let mapSection = document.querySelector('#map_section');
+let infoSection = document.querySelector('#info_section');
 let journalCard = document.querySelector('#journals_section template');
 let paperCard = document.querySelector('#papers_section template');
-let gridSvg = document.querySelector('#grid');
-let tooltip = document.querySelector('#tooltip');
+let mapSvg = document.querySelector('#map');
+let mapGrid = document.querySelector('#grid');
+let marker = document.querySelector('#marker');
+let paperCount = document.querySelector('#paper_count');
+let topJournals = document.querySelector('#top_journals');
+let topPcs = document.querySelector('#top_pcs');
 let appTitle = document.title;
 
 // global var to hold search box text
@@ -35,6 +38,7 @@ let query = '';
 // global vars to hold results from backend query
 let journals = [];
 let papers = [];
+let coordinates = {};
 
 // when user types into search box
 const onType = () => {
@@ -90,6 +94,10 @@ const onSearch = async (event) => {
     // extract journals and papers from backend results
     journals = neighbors.journal_neighbors || [];
     papers = neighbors.paper_neighbors || [];
+    coordinates = {
+      x: neighbors['2d_coord'].dim1,
+      y: neighbors['2d_coord'].dim2
+    };
 
     // if results empty, throw an error
     if (!journals.length || !papers.length) throw Error('Empty response');
@@ -236,7 +244,6 @@ const showResults = () => {
   errorMessage.style.display = 'none';
   journalsSection.style.display = 'block';
   papersSection.style.display = 'block';
-  mapSection.style.display = 'block';
 
   // delete any existing result elements
   const journalCards = document.querySelectorAll('#journals_section .card');
@@ -247,6 +254,10 @@ const showResults = () => {
   // make new journal and paper cards
   makeCards('journals', journals, journalCard, journalsSection);
   makeCards('papers', papers, paperCard, papersSection);
+
+  // place marker on map for queried preprint
+  marker.setAttribute('cx', coordinates.x);
+  marker.setAttribute('cy', coordinates.y);
 };
 
 // load logo inline for animation on hover and loading
@@ -287,25 +298,8 @@ const makeMap = async () => {
   // fetch journal bin data
   let data = await (await fetch(mapData)).json();
 
-  // extract coordinates
-  let xs = data.map((d) => d.x);
-  let ys = data.map((d) => d.y);
-  // get extreme values
-  let maxX = Math.max(...xs);
-  let minX = Math.min(...xs);
-  let maxY = Math.max(...ys);
-  let minY = Math.min(...ys);
-  // get range and center of coordinates
-  let range = Math.max(Math.abs(maxX - minX), Math.abs(maxY - minY)) + 1;
-  let offsetX = -(maxX + minX) / 2;
-  let offsetY = -(maxY + minY) / 2;
-
-  // how big, in svg units, to draw each cell
-  let size = 100 / range;
-  let overlap = -0.1;
-
-  // pre compute count ranges
-  let counts = data.map((d) => d.count);
+  // pre compute paper count ranges
+  let counts = data.map((d) => d.papers);
   let minCount = Math.min(...counts);
   let maxCount = Math.max(...counts);
   let countRange = maxCount - minCount;
@@ -318,95 +312,63 @@ const makeMap = async () => {
     cell.setAttribute('class', 'cell');
 
     // position and size cell
-    cell.setAttribute('x', (d.x + offsetX) * size - size / 2 - overlap / 2);
-    cell.setAttribute('y', (d.y + offsetY) * size - size / 2 - overlap / 2);
-    cell.setAttribute('width', size + overlap);
-    cell.setAttribute('height', size + overlap);
+    cell.setAttribute('x', d.x - 0.5);
+    cell.setAttribute('y', d.y - 0.5);
+    cell.setAttribute('width', 1);
+    cell.setAttribute('height', 1);
 
-    // put select info into cell element as string
-    const info = {
-      'Papers': d.count.toLocaleString(),
-      'Top Journals': Object.entries(d.journal || {})
-        .sort((a, b) => b[1] - a[1])
-        .slice(0, 5)
-        .map(
-          (journal) =>
-            journal[0] + ' (' + journal[1].toLocaleString() + ' papers)'
-        )
-    };
-    cell.setAttribute('data-info', JSON.stringify(info));
+    // put info into cell element as string
+    cell.setAttribute('data-info', JSON.stringify(d));
 
-    // trigger tooltip on hover of cell
-    cell.addEventListener('mouseover', openTooltip);
+    // show cell info on click
+    cell.addEventListener('click', showCellInfo);
 
-    // color cell according to count
-    let strength = (d.count - minCount) / countRange; // normalized (0-1) count
+    // color cell according to paper count
+    let strength = (d.papers - minCount) / countRange; // normalized (0-1) count
     cell.setAttribute('fill-opacity', 0.25 + strength * 0.75);
 
     // add cell to svg
-    gridSvg.append(cell);
+    mapGrid.append(cell);
   }
 
-  gridSvg.addEventListener('mouseleave', closeTooltip);
+  // center svg view on plotted cells
+  const { x, y, width, height } = mapGrid.getBBox();
+  mapSvg.setAttribute('viewBox', [x, y, width, height].join(' '));
 };
 makeMap();
 
-// set all cells' hover state to false
-const unhoverCells = () => {
-  document
-    .querySelectorAll('.cell[data-hover="true"]')
-    .forEach((cell) => (cell.dataset.hover = false));
-};
-
-// when user hovers over cell in map
-const openTooltip = (event) => {
-  // use element
-  let use = event.currentTarget;
-  // rect element
-  let rect = event.target;
-
-  // get info previously put into cell element
-  let info = use.dataset?.info;
+// show cell info when user clicks on it
+const showCellInfo = () => {
+  // get data from cell attribute
+  let info = event.target.dataset.info;
   if (!info) return;
-
-  // decode string as json object
   info = JSON.parse(info);
 
-  // set cell hover state
-  unhoverCells();
-  use.dataset.hover = true;
+  // select clicked cell
+  document
+    .querySelectorAll('.cell')
+    .forEach((el) => (el.dataset.selected = false));
+  event.target.dataset.selected = true;
 
-  // position tooltip relative to cell
-  let bbox = rect.getBoundingClientRect();
-  tooltip.style.left = bbox.left + bbox.width / 2;
-  tooltip.style.top = bbox.top + bbox.height / 2;
+  console.log(info);
+  // show info section
+  infoSection.style.display = 'block';
 
-  // make tooltip visible
-  tooltip.style.display = 'block';
-
-  // set tooltip content
-  let html = '';
-  for (const [key, value] of Object.entries(info)) {
-    html += '<div class="key">' + key + '</div>';
-    html += '<div class="value">';
-    if (Array.isArray(value)) {
-      for (const entry of value) {
-        html += entry;
-        html += '<br>';
-      }
-    } else html += value;
-    html += '</div>';
-  }
-  tooltip.innerHTML = html;
-};
-
-// when user unhovers off of a cell in the map
-const closeTooltip = () => {
-  // hide tooltip
-  tooltip.style.display = 'none';
-
-  // unhover cells
-  unhoverCells();
+  // format and display info
+  paperCount.innerHTML = info.papers.toLocaleString();
+  topJournals.innerHTML = info.journals
+    .map(({ name, count }) => {
+      count = count.toLocaleString();
+      return `${name} <i>(${count} papers)</i>`;
+    })
+    .join('<br>');
+  topPcs.innerHTML = info.pcs
+    .map(({ name, score }) => {
+      const url = cloudImages.replace('XX', name);
+      score = score.toFixed(2);
+      return `<a href="${url}" target="_blank">${name} <i>(${score})</i></a>`;
+    })
+    .join('<br>');
 };
 
 // add trigger listeners
