aa652f584ffc48d0a8c900402ee9fe5f967f0e17
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-08-15 01:42:50 -0400

    update

--- frontend/src/map.js
+++ frontend/src/map.js
@@ -7,22 +7,64 @@ import { useRef } from 'react';
 import './map.css';
 
 const mapData = './data/plot.json';
-const cloudImages =
+const pcImages =
   'https://raw.githubusercontent.com/greenelab/annorxiver/master/biorxiv/pca_association_experiment/output/word_pca_similarity/figures/pca_XX_cossim_word_cloud.png';
+
+const startImage = 1;
+const endImage = 50;
 
 const cellSize = 10;
 
 export default ({ coordinates }) => {
-  const svg = useRef();
   const [cells, setCells] = useState([]);
-  const [viewBox, setViewBox] = useState('');
   const [selectedCell, setSelectedCell] = useState(null);
+  const [selectedPc, setSelectedPc] = useState(null);
 
   useEffect(() => {
     const getMapData = async () =>
       setCells(await (await fetch(mapData)).json());
     getMapData();
   }, []);
+
+  return (
+    <section>
+      <h3>Map of PubMed Central</h3>
+      <PcButtons {...{ selectedPc, setSelectedPc }} />
+      <Map {...{ cells, selectedCell, setSelectedCell }} />
+      {coordinates.x && coordinates.y && <Marker {...{ coordinates }} />}
+      {selectedCell && <SelectedCellDetails {...{ selectedCell }} />}
+    </section>
+  );
+};
+
+const PcButtons = ({ selectedPc, setSelectedPc }) => {
+  const getSrc = (index) =>
+    pcImages.replace('XX', String(index).padStart(2, '0'));
+
+  const images = [];
+  for (let index = startImage; index < endImage; index++) {
+    images.push(
+      <button
+        key={index}
+        className='pc_button'
+        data-selected={selectedPc === index}
+        onClick={() => setSelectedPc(index)}
+      >
+        <img
+          src={getSrc(index)}
+          title={'Principal component ' + index}
+          alt={'Principal component ' + index}
+        />
+      </button>
+    );
+  }
+
+  return <p className='center'>{images}</p>;
+};
+
+const Map = ({ cells, selectedCell, setSelectedCell }) => {
+  const svg = useRef();
+  const [viewBox, setViewBox] = useState('');
 
   const counts = useMemo(() => cells.map((cell) => cell.papers), [cells]);
   const minCount = useMemo(() => Math.min(...counts), [counts]);
@@ -37,73 +79,62 @@ export default ({ coordinates }) => {
   }, [cells]);
 
   return (
-    <section>
-      <h3>Map of PubMed Central</h3>
-      <svg ref={svg} viewBox={viewBox || undefined} className='map'>
-        {cells
-          .sort((a, b) => {
-            if (a === selectedCell)
-              return 1;
-            if (b === selectedCell)
-              return -1;
-            return 0;
-          })
-          .map((cell, index) => (
-            <rect
-              key={index}
-              className='cell'
-              x={cell.x * cellSize - cellSize / 2}
-              y={cell.y * cellSize - cellSize / 2}
-              width={cellSize}
-              height={cellSize}
-              data-selected={cell === selectedCell}
-              fillOpacity={
-                0.25 + ((cell.papers - minCount) / countRange) * 0.75
-              }
-              onClick={() => setSelectedCell(cell)}
-            />
-          ))}
-        {coordinates.x && coordinates.y && (
-          <circle
-            className='marker'
-            cx={coordinates.x * cellSize}
-            cy={coordinates.y * cellSize}
-            r={cellSize / 3}
-          />
-        )}
-      </svg>
-      {selectedCell && (
-        <div>
-          <h4>Papers</h4>
-          <p>{selectedCell.papers.toLocaleString()}</p>
-          <h4>Top Journals</h4>
-          <p>
-            {selectedCell.journals.map(({ name, count }, index) => (
-              <div key={index} className='cell_detail_row'>
-                <span>{name}</span>
-                <span>{count.toLocaleString()} papers</span>
-                <br />
-              </div>
-            ))}
-          </p>
-          <h4>Top Principal Components</h4>
-          <p>
-            {selectedCell.pcs.map(({ name, score }, index) => (
-              <div key={index} className='cell_detail_row'>
-                <a
-                  href={cloudImages.replace('XX', name)}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {name}
-                </a>
-                <span>{score.toFixed(2)} score</span>
-                <br />
-              </div>
-            ))}
-          </p>
-        </div>
-      )}
-    </section>
+    <svg ref={svg} viewBox={viewBox || undefined} className='map'>
+      {cells.map((cell, index) => (
+        <rect
+          key={index}
+          className='cell'
+          x={cell.x * cellSize - cellSize / 2}
+          y={cell.y * cellSize - cellSize / 2}
+          width={cellSize}
+          height={cellSize}
+          data-selected={cell === selectedCell}
+          fillOpacity={0.25 + ((cell.papers - minCount) / countRange) * 0.75}
+          onClick={() => setSelectedCell(cell)}
+        />
+      ))}
+    </svg>
   );
 };
+
+const Marker = ({ coordinates }) => (
+  <circle
+    className='marker'
+    cx={coordinates.x * cellSize}
+    cy={coordinates.y * cellSize}
+    r={cellSize / 3}
+  />
+);
+
+const SelectedCellDetails = ({ selectedCell }) => (
+  <div>
+    <h4>Papers</h4>
+    <p>{selectedCell.papers.toLocaleString()}</p>
+    <h4>Top Journals</h4>
+    <p>
+      {selectedCell.journals.map(({ name, count }, index) => (
+        <span key={index} className='cell_detail_row'>
+          <span>{name}</span>
+          <span>{count.toLocaleString()} papers</span>
+          <br />
+        </span>
+      ))}
+    </p>
+    <h4>Top Principal Components</h4>
+    <p>
+      {selectedCell.pcs.map(({ name, score }, index) => (
+        <span key={index} className='cell_detail_row'>
+          <a
+            href={pcImages.replace('XX', name)}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {name}
+          </a>
+          <span>{score.toFixed(2)} score</span>
+          <br />
+        </span>
+      ))}
+    </p>
+  </div>
+);
