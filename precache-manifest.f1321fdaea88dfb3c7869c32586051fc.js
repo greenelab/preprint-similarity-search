d26110b949dc9846249b20a5b41d634c03b96412
vincerubinetti <vincerubinetti@users.noreply.github.com>
2020-08-16 23:04:18 +0000

    deploy: 5790e65943cf60b994e8625d0f9d284867c66996

---
+++ precache-manifest.f1321fdaea88dfb3c7869c32586051fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f949d7899e8bf4cd995cef3eecbd8109",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27891dc23f109dd27289",
+    "url": "./static/css/main.4505d155.chunk.css"
+  },
+  {
+    "revision": "b44fd91c466c7faeb98c",
+    "url": "./static/js/2.e2752ec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e2752ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27891dc23f109dd27289",
+    "url": "./static/js/main.7e67f066.chunk.js"
+  },
+  {
+    "revision": "a0b3c4a99ef04e206b58",
+    "url": "./static/js/runtime-main.fdc69087.js"
+  },
+  {
+    "revision": "e444815ccb02a6a676eb40f62632ce96",
+    "url": "./static/media/logo.e444815c.svg"
+  }
+]);
