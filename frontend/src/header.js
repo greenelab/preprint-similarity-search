30723283634a34d00e55072bcb41b4ce800ea7b6
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-08-16 17:52:13 -0400

    Convert frontend app to react (#27)
    - convert entire app to react app
    - move frontend app to frontend folder to clean up repo
    - add word cloud (principal component) image thumbnails with tooltip enlargements. selecting them doesn't do anything yet
    - reformat selected map cell info

---
+++ frontend/src/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { ReactComponent as Logo } from './logo.svg';
+
+import './header.css';
+
+// header component
+
+export default () => (
+  <header>
+    <section>
+      <h1>AnnoRxver</h1>
+      <h2>Journal Recommender</h2>
+      <Logo className='logo' />
+    </section>
+  </header>
+);
