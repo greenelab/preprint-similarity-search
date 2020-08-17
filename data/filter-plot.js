2d987e642c45b8ef927c0f1fca293797056ac2d0
vincerubinetti <vincerubinetti@users.noreply.github.com>
2020-08-17 21:52:36 +0000

    deploy: 6eb6f143df9e10f16ccab454f2d58b6c340474e9

--- data/filter-plot.js
+++ data/filter-plot.js
@@ -1,32 +1,33 @@
-// open a browser then open the console with f12
-// type "data = ", then paste the full contents of plot-full.json
-// then run the rest of these commands
+// helper script to consolidate and format the plot data
 
-// remove data points with 0 counts
-data = data.filter((d) => d.count);
-// rebuild object with only needed info
-data = data.map((d) => ({
-  // coordinates
-  x: d.x,
-  y: d.y,
-  // # of papers, to string with comma
-  papers: d.count,
-  // journals
-  journals: Object.entries(d.journal || {})
-    // sort by paper count
-    .sort((a, b) => b[1] - a[1])
-    // cut list to top 5
-    .slice(0, 5)
-    // put sensible field names
-    .map(([name, count]) => ({ name, count })),
-  // principal components
-  pcs: d.pc
-    // sort by absolute value of score
-    .sort((a, b) => Math.abs(b.score) - Math.abs(a.score))
-    // cut to top 5
-    .slice(0, 5)
-    // put sensible field names
-    .map(({ pc, score }) => ({ name: pc, score }))
-}));
-// put filtered data into clipboard to be pasted into plot.json
-copy(JSON.stringify(data));
+// open a browser and open the dev console with f12
+// type "data = ", then paste the full contents of the input json file
+// run the following commands and the output json will be put on the clipboard
+
+newData = data
+  // remove data points with 0 counts
+  .filter(d => d.count)
+  // rebuild object with only needed info
+  .map(d => ({
+    // coordinates
+    x: d.x,
+    y: d.y,
+    // # of papers, to string with comma
+    papers: d.count,
+    // journals
+    journals: Object.entries(d.journal || {})
+      // put sensible field names
+      .map(([name, count]) => ({ name, count }))
+      // sort by paper count
+      .sort((a, b) => b.count - a.count)
+      // cut list to top 5 to save space
+      .slice(0, 5),
+    // principal components
+    pcs: d.pc
+      // sort by absolute value of score
+      .sort((a, b) => Math.abs(b.score) - Math.abs(a.score))
+      // put sensible field names
+      .map(({ pc, score }) => ({ name: pc, score }))
+  }));
+// put filtered data into clipboard
+copy(JSON.stringify(newData));
--- precache-manifest.4462b473bf76d9f357f0ffb636ac94f4.js
+++ precache-manifest.4462b473bf76d9f357f0ffb636ac94f4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e5e59ee00a2613193df2ba286244f4a",
+    "revision": "ce1867b27720a5a1b16d134c833b294c",
     "url": "./index.html"
   },
   {
-    "revision": "219bbbc6352d24451167",
-    "url": "./static/css/main.d3ec06be.chunk.css"
+    "revision": "4cbc77c0de2584a2b7d3",
+    "url": "./static/css/main.5a9e2e83.chunk.css"
   },
   {
-    "revision": "b44fd91c466c7faeb98c",
-    "url": "./static/js/2.e2752ec0.chunk.js"
+    "revision": "3a190b704b7b6ec95737",
+    "url": "./static/js/2.cd80c4c7.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "./static/js/2.e2752ec0.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.cd80c4c7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "219bbbc6352d24451167",
-    "url": "./static/js/main.a53a254b.chunk.js"
+    "revision": "4cbc77c0de2584a2b7d3",
+    "url": "./static/js/main.3db2e3bb.chunk.js"
   },
   {
     "revision": "a0b3c4a99ef04e206b58",
