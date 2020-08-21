f6452f7d9d9df35eb81b0eabfbbd011f5f2eab5c
David Nicholson <dnicholson329@gmail.com>
2020-08-21 15:35:31 -0400

    Fixed parsing error #2:

--- server/word_vectors.py
+++ server/word_vectors.py
@@ -2,7 +2,8 @@ import numpy as np
 import pickle
 from io import BytesIO, StringIO
 from gensim.parsing.preprocessing import remove_stopwords
-from pdfminer.high_level import extract_text, extract_text_to_fp
+from pdfminer.high_level import eextract_text_to_fp
+from pdfminer.layout import LAParams
 
 word_model_wv = pickle.load(open('data/word2vec_model/word_model.wv.pkl', 'rb'))
 
@@ -21,7 +22,8 @@ def parse_content(content, maxpages=999):
     # Use this function to write pdf text to the file stream
     extract_text_to_fp(
         BytesIO(content), text_to_process, 
-        disable_caching=True, maxpages=maxpages
+        disable_caching=True, maxpages=maxpages,
+        laparams=LAParams()
     )
     
     # Convert text to word vectors and continue processing
