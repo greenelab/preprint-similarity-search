44cb7a24459b4d43126b3ba6b40cc31e472c534f
David Nicholson <dnicholson329@gmail.com>
2020-09-09 15:05:24 -0400

    Support for medrxiv is now online

--- server/document_downloader.py
+++ server/document_downloader.py
@@ -2,7 +2,6 @@ import requests
 from flask_restful import abort
 from utils import server_log
 
-
 def get_doi_content(user_doi):
     """
     This function is designed to render the paper-journal
@@ -11,35 +10,26 @@ def get_doi_content(user_doi):
     Args:
         user_doi - a biorxiv doi that grabs the most current version of a preprint
     """
+    
+    # Try pinging biorxiv server first
+    content = ping_biorxiv(user_doi)
+    pdf_url = f"http://biorxiv.org/content"
+    
+    # If no match found try medrxiv
+    if content is None: 
+        
+        content = ping_medrxiv(user_doi)
+        pdf_url = f"http://medrxiv.org/content"
 
-    api_url = f"https://api.biorxiv.org/details/biorxiv/{user_doi}"
-    try:
-        response = requests.get(api_url)
-    except Exception as e:
-        message = f"Cannot connect to {api_url}"
-        server_log(f"{message}: {e}\n")
-        abort(404, message=message)
-
-    if response.status_code != 200:
-        message = f"Invalid response from {api_url}"
-        server_log(f"{message}\n")
-        abort(response.status_code, message=message)
-
-    try:
-        content = response.json()
-    except Exception as e:
-        message = f"Cannot convert response from {api_url} to json format"
-        server_log(f"{message}: {e}\n")
-        abort(404, message=message)
-
-    if len(content['collection']) < 1:
-        message = f"Cannot find bioRxiv document: {user_doi}"
-        server_log(f"{message}\n")
-        abort(404, message=message)
-
+        # If no match at all then raise the red flag
+        if content is None:
+            message = f"Cannot find document {user_doi} in biorxiv nor medrxiv."
+            server_log(f"{message}\n")
+            abort(404, message=message)
+    
     # Grab latest version of PDF file
     latest_version = content['collection'][-1]['version']
-    pdf_url = f"http://biorxiv.org/content/{user_doi}v{latest_version}.full.pdf"
+    pdf_url = f"{pdf_url}/{user_doi}v{latest_version}.full.pdf"
     try:
         response = requests.get(pdf_url)
     except Exception as e:
@@ -53,3 +43,70 @@ def get_doi_content(user_doi):
         abort(response.status_code, message=message)
 
     return response.content
+
+def ping_biorxiv(doi):
+    """
+    This function pings biorxiv to see if doi exists
+    within their repository
+    
+    Args:
+        doi - a doi that grabs the most current version of a preprint
+    """
+    
+    api_url = f"https://api.biorxiv.org/details/biorxiv/{doi}"
+    try:
+        response = requests.get(api_url)
+    except Exception as e:
+        message = f"Cannot connect to {api_url}"
+        server_log(f"{message}: {e}\n")
+        abort(404, message=message)
+
+    if response.status_code != 200:
+        message = f"Invalid response from {api_url}"
+        server_log(f"{message}\n")
+        abort(response.status_code, message=message)
+    
+    try:
+        content = response.json()
+    except Exception as e:
+        message = f"Cannot convert response from {api_url} to json format"
+        server_log(f"{message}: {e}\n")
+        abort(404, message=message)
+
+    if len(content['collection']) < 1:
+        return None
+    
+    return content
+    
+def ping_medrxiv(doi):
+    """
+    This function pings medrxiv to see if doi exists
+    within their repository
+    
+    Args:
+        doi - a doi that grabs the most current version of a preprint
+    """
+    api_url = f"https://api.biorxiv.org/details/medrxiv/{doi}"
+    try:
+        response = requests.get(api_url)
+    except Exception as e:
+        message = f"Cannot connect to {api_url}"
+        server_log(f"{message}: {e}\n")
+        abort(404, message=message)
+
+    if response.status_code != 200:
+        message = f"Invalid response from {api_url}"
+        server_log(f"{message}\n")
+        abort(response.status_code, message=message)
+    
+    try:
+        content = response.json()
+    except Exception as e:
+        message = f"Cannot convert response from {api_url} to json format"
+        server_log(f"{message}: {e}\n")
+        abort(404, message=message)
+
+    if len(content['collection']) < 1:
+        return None
+    
+    return content
