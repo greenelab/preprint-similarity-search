c27666494576b6f3e1785bad118883cfc0ff904a
David Nicholson <dnicholson329@gmail.com>
2021-03-05 12:33:43 -0500

    added xml parser

--- server/document_downloader.py
+++ server/document_downloader.py
@@ -1,67 +1,86 @@
+from pathlib import Path
+
 import requests
 from flask_restful import abort
 from utils import server_log
+
 
 def get_doi_content(user_doi):
     """
     This function is designed to render the paper-journal
     network for the user given a biorxiv doi
-
     Args:
         user_doi - a biorxiv doi that grabs the most current version of a preprint
     """
-    
+
     # Try pinging biorxiv server first
     content = ping_biorxiv_or_medrxiv(user_doi, server="biorxiv")
-    pdf_url = f"http://biorxiv.org/content"
-    
+    doc_url = f"http://biorxiv.org/content"
+
     # If no match found try medrxiv
-    if content is None: 
+    if content is None:
         content = ping_biorxiv_or_medrxiv(user_doi, server="medrxiv")
-        pdf_url = f"http://medrxiv.org/content"
+        doc_url = f"http://medrxiv.org/content"
 
         # If no match at all then raise the red flag
         if content is None:
             message = f"Cannot find document {user_doi} in either biorxiv or medrxiv."
             server_log(f"{message}\n")
             abort(404, message=message)
-    
-    latest_paper = content['collection'][-1]
-    
+
+    latest_paper = content["collection"][-1]
+
     paper_metadata = {
-        "title": latest_paper['title'],
-        "authors": latest_paper['authors'],
-        "doi": latest_paper['doi'],
-        "accepted_date": latest_paper['date'],
-        "publisher": "Cold Spring Harbor Laboratory"
+        "title": latest_paper["title"],
+        "authors": latest_paper["authors"],
+        "doi": latest_paper["doi"],
+        "accepted_date": latest_paper["date"],
+        "publisher": "Cold Spring Harbor Laboratory",
     }
-    
-    # Grab latest version of PDF file
-    pdf_url = f"{pdf_url}/{user_doi}v{latest_paper['version']}.full.pdf"
+
+    # Grab latest version of the XML file if available
+    accepted_date = latest_paper["date"].replace("-", "/")
+    file_url = f"{doc_url}/early/{accepted_date}/{str(Path(user_doi).stem)}.source.xml"
+    xml_found = False
+
     try:
-        response = requests.get(pdf_url)
+        response = requests.get(file_url)
+        if response.status_code == 200:
+            xml_found = True
+
     except Exception as e:
-        message = f"Cannot connect to {pdf_url}"
+        message = f"Cannot connect to {file_url}"
         server_log(f"{message}: {e}\n")
-        abort(404, message=message)
+
+    # If xml not found then use PDF version
+    if not xml_found:
+
+        # Grab latest version of PDF file
+        file_url = f"{doc_url}/{user_doi}v{latest_paper['version']}.full.pdf"
+        try:
+            response = requests.get(file_url)
+        except Exception as e:
+            message = f"Cannot connect to {file_url}"
+            server_log(f"{message}: {e}\n")
+            abort(404, message=message)
 
     if response.status_code != 200:
-        message = f"Invalid response from {pdf_url}"
+        message = f"Invalid response from {file_url}"
         server_log(f"{message}\n")
         abort(response.status_code, message=message)
 
-    return response.content, paper_metadata
+    return response.content, paper_metadata, xml_found
+
 
 def ping_biorxiv_or_medrxiv(doi, server="biorxiv"):
     """
     This function pings biorxiv or medrxiv to see if doi exists
     within their repository
-    
     Args:
         doi - a doi that grabs the most current version of a preprint
     """
     api_url = f"https://api.biorxiv.org/details/{server}/{doi}"
-    
+
     try:
         response = requests.get(api_url)
     except Exception as e:
@@ -73,7 +92,7 @@ def ping_biorxiv_or_medrxiv(doi, server="biorxiv"):
         message = f"Invalid response from {api_url}"
         server_log(f"{message}\n")
         abort(response.status_code, message=message)
-    
+
     try:
         content = response.json()
     except Exception as e:
@@ -81,7 +100,7 @@ def ping_biorxiv_or_medrxiv(doi, server="biorxiv"):
         server_log(f"{message}: {e}\n")
         abort(404, message=message)
 
-    if len(content['collection']) < 1:
+    if len(content["collection"]) < 1:
         return None
-    
+
     return content
--- server/word_vectors.py
+++ server/word_vectors.py
@@ -1,40 +1,83 @@
-import numpy as np
-import pickle
 from io import BytesIO
+import pickle
+
+import lxml.etree as ET
+import numpy as np
 import spacy
 import fitz
 
 nlp = spacy.load("en_core_web_sm")
-word_model_wv = pickle.load(open('data/word2vec_model/word_model.wv.pkl', 'rb'))
+word_model_wv = pickle.load(open("data/word2vec_model/word_model.wv.pkl", "rb"))
+filter_tag_list = [
+    "sc",
+    "italic",
+    "xref",
+    "label",
+    "sub",
+    "sup",
+    "inline-formula",
+    "fig",
+    "disp-formula",
+    "bold",
+    "table-wrap",
+    "table",
+    "thead",
+    "tbody",
+    "caption",
+    "tr",
+    "td",
+]
+parser = ET.XMLParser(encoding="UTF-8", recover=True)
 
-def parse_content(content, maxpages=999):
+
+def parse_content(content, xml_file=True):
     """
     Parses input content and returns a vector based on the pre-loaded
     `word_model_wv`.
-
     Args:
         content - a PDF file's contents to be parsed
     """
-    
-    # Have a faux file stream for parsing
-    text_to_process = BytesIO(content)
-    
-    # Use this function to write pdf text to the file stream
-    pdf_parser = fitz.open(
-        stream=text_to_process, 
-        filetype="pdf"
-    )
-    
-    # Convert text to word vectors and continue processing
     word_vectors = []
-    for page in pdf_parser:
-        tokens = list(map(str, nlp(page.getText())))
-        word_vectors += [
-            word_model_wv[tok]
-            for tok in tokens
-            if tok in word_model_wv and tok not in nlp.Defaults.stop_words
-        ]
+    if xml_file:
+        biorxiv_xpath_str = (
+            "//abstract/p|//abstract/title|//body/sec//p|//body/sec//title"
+        )
+
+        # Parse the xml document
+        root = ET.fromstring(content, parser=parser)
+
+        # Process xml without specified tags
+        ET.strip_tags(root, *filter_tag_list)
+
+        all_tags = root.xpath(biorxiv_xpath_str)
+        text_to_process = list(map(lambda x: list(x.itertext()), list(all_tags)))
+
+        for text in text_to_process:
+            tokens = list(map(str, nlp(text[0])))
+
+            word_vectors += [
+                word_model_wv[tok]
+                for tok in tokens
+                if tok in word_model_wv and tok not in nlp.Defaults.stop_words
+            ]
+    else:
+
+        # Have a faux file stream for parsing
+        text_to_process = BytesIO(content)
+
+        # Use this function to write pdf text to the file stream
+        pdf_parser = fitz.open(stream=text_to_process, filetype="pdf")
+
+        # Convert text to word vectors and continue processing
+        for page in pdf_parser:
+            tokens = list(map(str, nlp(page.getText())))
+
+            word_vectors += [
+                word_model_wv[tok]
+                for tok in tokens
+                if tok in word_model_wv and tok not in nlp.Defaults.stop_words
+            ]
 
     word_embedd = np.stack(word_vectors)
-    query_vec = word_embedd.mean(axis=0)[np.newaxis,:]
+    query_vec = word_embedd.mean(axis=0)[np.newaxis, :]
     return query_vec
