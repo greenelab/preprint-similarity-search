b9a199c4b7f70e34681a62569a3ed7840676c6a4
David Nicholson <dnicholson329@gmail.com>
2021-04-15 11:30:57 -0400

    Updated script to use lemma_ and updated sauice model

--- server/auto_updater/get_square_bins.R
+++ server/auto_updater/get_square_bins.R
@@ -1,9 +1,9 @@
 library(ggplot2)
 
-bin_num <- 50
+bin_num = 65
 g <- (
     ggplot(data_df, aes(x=dim1, y=dim2))
-    + geom_bin2d(bins=bin_num, binwidth=0.85)
+    + geom_bin2d(bins=bin_num)
     + theme(legend.position="left")
 )
 square_plot_df <- ggplot_build(g)$data[[1]]
