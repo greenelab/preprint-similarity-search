dff096d767cbed5d4c82d5a4213f028dc84dfad6
dongbohu <dongbo.hu@gmail.com>
2021-06-01 11:58:33 -0400

    update

--- auto_updater/journal_cenetroid.py
+++ auto_updater/journal_cenetroid.py
@@ -3,7 +3,6 @@
 # Generate journal centroid dataset
 
 import csv
-#from collections import defaultdict
 from pathlib import Path
 
 import numpy as np
@@ -23,7 +22,7 @@ def generate_journal_centroid(paper_embeddings_filename, output_filename):
       * output_filename - output file that contains the journal centroids
     """
 
-    journal_centroid = dict()  # defaultdict(dict)
+    journal_centroid = dict()
 
     # Read input paper embeddings file
     with open(paper_embeddings_filename, "r") as ifh:
